eabd682b9f5285b046a778a4034cdb8b23efc62a
USAxioms <michael@usaxioms.com>
2026-07-21 05:00:17 -0700

    Create AppRouter.vue

---
+++ src/components/AppRouter.vue
@@ -0,0 +1,10 @@
+<template>
+  <component :is="activeView" />
+</template>
+
+<script setup>
+import { computed } from 'vue'
+import { Router } from '@/router/router.js'
+
+const activeView = computed(() => Router.routes[Router.current])
+</script>
