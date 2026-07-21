e228c24840c8952332a098cb107b5443d54f50d4
USAxioms <michael@usaxioms.com>
2026-07-21 00:21:26 -0700

    Create Home.vue

---
+++ frontend/src/pages/Home.vue
@@ -0,0 +1,44 @@
+<template>
+  <Layout :showSidebar="true" sidebarTitle="Navigation">
+    <template #nav-left>
+      <strong>ADVANCEER</strong>
+    </template>
+
+    <template #sidebar>
+      <ul class="menu">
+        <li><a href="/">Dashboard</a></li>
+        <li><a href="/settings">Settings</a></li>
+      </ul>
+    </template>
+
+    <Card title="Welcome">
+      <p>This is the home page.</p>
+    </Card>
+  </Layout>
+</template>
+
+<script setup>
+import Layout from '../layouts/Layout.vue'
+import Card from '../components/Card.vue'
+</script>
+
+<style scoped>
+.menu {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+
+.menu li {
+  margin-bottom: 0.5rem;
+}
+
+.menu a {
+  color: #e5e7eb;
+  text-decoration: none;
+}
+
+.menu a:hover {
+  text-decoration: underline;
+}
+</style>
