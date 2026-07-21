9cf3c2f47d026643c41a34478a790f6445b493f3
USAxioms <michael@usaxioms.com>
2026-07-21 10:34:25 -0700

    Create AppExecution.js

---
+++ src/execution/AppExecution.js
@@ -0,0 +1,4 @@
+import { CognitiveExecutionEngine } from '@/engine/CognitiveExecutionEngine.js'
+import { Reasoning } from '@/reasoning/AppReasoning.js'
+import { R3 } from '@/r3/AppR3.js'
+import { State
