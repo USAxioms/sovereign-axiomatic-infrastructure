8c115103c7b0c378c05a5d2035274540fd14da58
USAxioms <michael@usaxioms.com>
2026-07-21 04:40:09 -0700

    Create Breadcrumbs.vue

---
+++ src/components/Breadcrumbs.vue
@@ -0,0 +1,43 @@
+<template>
+  <nav class="breadcrumbs">
+    <span
+      v-for="(item, index) in items"
+      :key="index"
+      class="crumb"
+      @click="$emit('navigate', index)"
+    >
+      {{ item }}
+      <span v-if="index < items.length - 1" class="separator">/</span>
+    </span>
+  </nav>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+
+defineProps({
+  items: {
+    type: Array,
+    required: true
+  }
+})
+</script>
+
+<style scoped>
+.breadcrumbs {
+  display: flex;
+  align-items: center;
+  font-size: 0.9rem;
+  color: #555;
+}
+
+.crumb {
+  cursor: pointer;
+  padding: 0 0.25rem;
+}
+
+.separator {
+  margin: 0 0.25rem;
+  color: #aaa;
+}
+</style>
