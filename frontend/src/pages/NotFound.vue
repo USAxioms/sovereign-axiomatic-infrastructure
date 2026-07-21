db68159540ea6784e09f1df5a38fcbabab20fc06
USAxioms <michael@usaxioms.com>
2026-07-21 00:36:49 -0700

    Create NotFound.vue

---
+++ frontend/src/pages/NotFound.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="notfound-container">
+    <Card title="404 - Page Not Found">
+      <p>The page you are looking for does not exist.</p>
+
+      <Button variant="primary" @click="goHome">
+        Go Back Home
+      </Button>
+    </Card>
+  </div>
+</template>
+
+<script setup>
+import Card from '../components/Card.vue'
+import Button from '../components/Button.vue'
+
+function goHome() {
+  window.location.href = '/'
+}
+</script>
+
+<style scoped>
+.notfound-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background: #f3f4f6;
+}
+</style>
