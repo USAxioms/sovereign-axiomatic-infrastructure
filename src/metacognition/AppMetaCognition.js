f0ff8df20ec0240ddf5e7cb18942b59bac774907
USAxioms <michael@usaxioms.com>
2026-07-21 13:11:32 -0700

    Create AppMetaCognition.js

---
+++ src/metacognition/AppMetaCognition.js
@@ -0,0 +1,6 @@
+import { MetaCognitionEngine } from '@/engine/MetaCognitionEngine.js'
+import { Ledger } from '@/ledger/AppLedger.js'
+
+export const MetaCog = new MetaCognitionEngine({
+  Ledger
+})
