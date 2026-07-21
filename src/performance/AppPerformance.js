6aa53c2cd21e86082e8739415e5431c870ac097d
USAxioms <michael@usaxioms.com>
2026-07-21 13:13:43 -0700

    Create AppPerformance.js

---
+++ src/performance/AppPerformance.js
@@ -0,0 +1,6 @@
+import { PerformanceVelocityOptimizer } from '@/engine/PerformanceVelocityOptimizer.js'
+import { Ledger } from '@/ledger/AppLedger.js'
+
+export const Performance = new PerformanceVelocityOptimizer({
+  Ledger
+})
