efe47dcbd41f5b45a176c1e239dee039a361f852
USAxioms <michael@usaxioms.com>
2026-07-21 03:38:12 -0700

    Create router.js

---
+++ src/router.js
@@ -0,0 +1,17 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+// Pages
+import Home from './pages/Home.vue'
+import NotFound from './pages/NotFound.vue'
+
+const routes = [
+  { path: '/', component: Home },
+  { path: '/:pathMatch(.*)*', component: NotFound }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
