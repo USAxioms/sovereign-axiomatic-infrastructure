08acad6fb07f11d19ff454453adefcb723bbb0ee
USAxioms <michael@usaxioms.com>
2026-07-21 04:56:38 -0700

    Create LayoutCol.vue

---
+++ src/components/LayoutCol.vue
@@ -0,0 +1,22 @@
+<template>
+  <div :class="classes">
+    <slot />
+  </div>
+</template>
+
+<script setup>
+import { defineProps, computed } from 'vue'
+
+const props = defineProps({
+  span: { type: Number, default: 12 },
+  md: { type: Number, default: null },
+  sm: { type: Number, default: null }
+})
+
+const classes = computed(() => {
+  const base = `col-${props.span}`
+  const md = props.md ? `col-md-${props.md}` : ''
+  const sm = props.sm ? `col-sm-${props.sm}` : ''
+  return `${base} ${md} ${sm}`
+})
+</script>
