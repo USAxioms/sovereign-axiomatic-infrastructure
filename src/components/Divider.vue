f28e02240e8d64bc36d48ba9165a2826efe9964f
USAxioms <michael@usaxioms.com>
2026-07-21 03:54:26 -0700

    Create Divider.vue

---
+++ src/components/Divider.vue
@@ -0,0 +1,16 @@
+<template>
+  <hr class="divider" />
+</template>
+
+<script setup>
+// Simple horizontal divider.
+// Used to separate content sections.
+</script>
+
+<style scoped>
+.divider {
+  border: none;
+  border-top: 1px solid #ddd;
+  margin: 1.5rem 0;
+}
+</style>
