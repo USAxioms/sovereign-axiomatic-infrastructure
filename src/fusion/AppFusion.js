0c7c391f20838e78296c5b2c56e06f2e25c6d98c
USAxioms <michael@usaxioms.com>
2026-07-21 15:48:33 -0700

    Create AppFusion.js

---
+++ src/fusion/AppFusion.js
@@ -0,0 +1,6 @@
+import { CognitiveFusionEngine } from '@/engine/CognitiveFusionEngine.js'
+import { Ledger } from '@/ledger/AppLedger.js'
+
+export const Fusion = new CognitiveFusionEngine({
+  Ledger
+})
