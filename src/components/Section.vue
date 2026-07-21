0690cbe99a4bce8a0d06fefd93a24e4b01001545
USAxioms <michael@usaxioms.com>
2026-07-21 03:53:13 -0700

    Create Section.vue

---
+++ src/components/Section.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="section">
+    <slot />
+  </section>
+</template>
+
+<script setup>
+// Simple section wrapper.
+// Helps organize page content into blocks.
+</script>
+
+<style scoped>
+.section {
+  margin-bottom: 2rem;
+}
+</style>
