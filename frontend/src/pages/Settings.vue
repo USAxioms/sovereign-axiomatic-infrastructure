4aff2f46971ea5233b065d17b51b2533ee09a1e1
USAxioms <michael@usaxioms.com>
2026-07-21 00:22:03 -0700

    Create Settings.vue

---
+++ frontend/src/pages/Settings.vue
@@ -0,0 +1,44 @@
+<template>
+  <Layout :showSidebar="true" sidebarTitle="Settings">
+    <template #nav-left>
+      <strong>ADVANCEER</strong>
+    </template>
+
+    <template #sidebar>
+      <ul class="menu">
+        <li><a href="/settings/profile">Profile</a></li>
+        <li><a href="/settings/system">System</a></li>
+      </ul>
+    </template>
+
+    <Card title="Settings">
+      <p>Adjust your preferences and system configuration here.</p>
+    </Card>
+  </Layout>
+</template>
+
+<script setup>
+import Layout from '../layouts/Layout.vue'
+import Card from '../components/Card.vue'
+</script>
+
+<style scoped>
+.menu {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+
+.menu li {
+  margin-bottom: 0.5rem;
+}
+
+.menu a {
+  color: #e5e7eb;
+  text-decoration: none;
+}
+
+.menu a:hover {
+  text-decoration: underline;
+}
+</style>
